39cb3f6842748193c1f36dc1fe35fb130d7eec2a
Gayathri M <gayathri.m352004@gmail.com>
2026-04-11 06:42:54 +0530

    changed code

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ function login(password) {
     if (password == "123456") {
         console.log("Login success");
     } else {
-        console.log("Login failed");
+        console.log("Login ");
     }
 }
